15b299b2266cf31009af269a8d990f87aa458463
ffabbio615 <fabiomarques.ads@gmail.com>
2025-03-18 16:03:07 -0300

    Atualização dos arquivos do projeto

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -34,8 +34,8 @@ function App() {
       <main>
         <section className={mode === 'portfolio' ? 'portfolio-section turn-right fade-in' : 'portfolio-section turn-left fade-out'}>
           <div className='image-container'>
-            <img className={contact === false ? 'personal-image' : 'personal-image fade-out'} src="/images/fotoPerfilAmbienteExterno.jpg" alt="Imagem de Perfil" />
-            <div className='contact-text'>
+            <img className={contact === false ? 'personal-image flip-reverse' : 'personal-image flip'} src="/images/fotoPerfilAmbienteExterno.jpg" alt="Imagem de Perfil" />
+            <div className={contact === false ? 'contact-text fade-out' : 'contact-text fade-in'}>
               <p>E-mail: fabiomarques.ads@gmail.com</p>
               <p>Whatsapp: (21) 99889-9535</p>
             </div>
@@ -63,7 +63,80 @@ function App() {
         </section>
 
         <section className={mode === 'portfolio' ? 'projects-section turn-right fade-out' : 'projects-section turn-left fade-in'}>
+        <div className='project-container'>
+          <img className='project-image-group' src="/images/primeMockup.png" alt="Mockup de projeto" />
+          <div className='project-informations-container'>
+            <p className='project-title'>Prime Language School</p>
+            <p className='project-subtitle'>Tecnologias utilizadas:</p>
+            <div className='tecnologies-icons'>
+              <img src="/images/viteIcon.png" alt="Ícone do GitHub" />
+              <img src="/images/javascriptIcon.svg" alt="Ícone do GitHub" />
+              <img src="/images/reactIcon.svg" alt="Ícone do GitHub" />
+              <img src="/images/cssIcon.png" alt="Ícone do GitHub" />
+              <img src="/images/sassIcon.svg" alt="Ícone do GitHub" />
+              <img src="/images/firebaseIcon.svg" alt="Ícone do GitHub" />
+            </div>
+            <a className='project-link'  target='_blank' href='https://prime-rust-psi.vercel.app/'>Acessar</a> 
+            <a className='github-link' target='_blank' href='https://github.com/ffabbio615/primeLanguageSchool'><img src="/images/gitHubIcon.svg" alt="Ícone do GitHub" /></a>
+          </div>
+        </div>
 
+        <div className='project-container'>
+          <img className='project-image-group' src="/images/capiwarasMockup.png" alt="Mockup de projeto" />
+          <div className='project-informations-container'>
+            <p className='project-title'>Capiwaras</p>
+            <p className='project-subtitle'>Tecnologias utilizadas:</p>
+            <div className='tecnologies-icons'>
+              <img src="/images/viteIcon.png" alt="Ícone do GitHub" />
+              <img src="/images/javascriptIcon.svg" alt="Ícone do GitHub" />
+              <img src="/images/reactIcon.svg" alt="Ícone do GitHub" />
+              <img src="/images/cssIcon.png" alt="Ícone do GitHub" />
+              <img src="/images/sassIcon.svg" alt="Ícone do GitHub" />
+            </div>
+            <a className='project-link' target='_blank' href='https://capiwaras.vercel.app/'>Acessar</a> 
+            <a className='github-link' target='_blank' href='https://github.com/ffabbio615/capiwaras'><img src="/images/gitHubIcon.svg" alt="Ícone do GitHub" /></a>
+          </div>
+        </div>
+
+        <div className='project-container'>
+          <img className='project-image-group' src="/images/russelMockup.png" alt="Mockup de projeto" />
+          <div className='project-informations-container'>
+            <p className='project-title'>Russel Shop</p>
+            <p className='project-subtitle'>Tecnologias utilizadas:</p>
+            <div className='tecnologies-icons'>
+              <img src="/images/viteIcon.png" alt="Ícone do GitHub" />
+              <img src="/images/javascriptIcon.svg" alt="Ícone do GitHub" />
+              <img src="/images/reactIcon.svg" alt="Ícone do GitHub" />
+              <img src="/images/cssIcon.png" alt="Ícone do GitHub" />
+              <img src="/images/sassIcon.svg" alt="Ícone do GitHub" />
+            </div>
+            <a className='project-link' target='_blank' href='https://russelshop.vercel.app/'>Acessar</a> 
+            <a className='github-link' target='_blank' href='https://github.com/ffabbio615/RusselShop'><img src="/images/gitHubIcon.svg" alt="Ícone do GitHub" /></a>
+          </div>
+        </div>
+
+        <div className='project-container'>
+          <div className='soon-container'>
+            <div>Em breve!</div>
+          </div>
+          <img className='project-image-group' src="/images/vocabaryMockup.png" alt="Mockup de projeto" />
+          <div className='project-informations-container'>
+            <p className='project-title'>Vocabary</p>
+            <p className='project-subtitle'>Tecnologias utilizadas:</p>
+            <div className='tecnologies-icons'>
+              <img src="/images/viteIcon.png" alt="Ícone do GitHub" />
+              <img src="/images/javascriptIcon.svg" alt="Ícone do GitHub" />
+              <img src="/images/typescriptIcon.svg" alt="Ícone do GitHub" />
+              <img src="/images/reactIcon.svg" alt="Ícone do GitHub" />
+              <img src="/images/cssIcon.png" alt="Ícone do GitHub" />
+              <img src="/images/sassIcon.svg" alt="Ícone do GitHub" />
+              <img src="/images/firebaseIcon.svg" alt="Ícone do GitHub" />
+            </div>
+            <a className='project-link' target='_blank' href='https://vocabary.vercel.app/'>Acessar</a> 
+            <a className='github-link' target='_blank' href='https://github.com/ffabbio615/vocabary'><img src="/images/gitHubIcon.svg" alt="Ícone do GitHub" /></a>
+          </div>
+        </div>
+        
         </section>
 
 
